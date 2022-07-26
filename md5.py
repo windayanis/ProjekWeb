3b6398ce7324e871e4cd0447953ba83230d5851d
windayanis <109908587+windayanis@users.noreply.github.com>
2022-07-26 13:38:47 +0700

    Update md5.py

--- md5.py
+++ md5.py
@@ -3,11 +3,13 @@ import hashlib
 
 def md_5():
     input_str = st.text_area("Masukan plain text")
-    if st.button("Hash"): 
-        if len(input_str) == 0:
-            st.error("Masukan Text!")
-        else:
-            hasil = hashlib.md5(input_str.encode())
-            st.info(hasil.hexdigest())
-            
-        
+    col1,col2 = st.columns([14,1])
+    with col2:
+        if st.button("Hash"): 
+            if len(input_str) == 0:
+                with col1:
+                    st.error("Masukan Text!")
+            else:
+                hasil = hashlib.md5(input_str.encode())
+                with col1:
+                    st.info(hasil.hexdigest())
