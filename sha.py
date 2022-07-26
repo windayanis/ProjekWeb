6e9491d7a264ad17658ed890e17bc2bee3eb916c
windayanis <109908587+windayanis@users.noreply.github.com>
2022-07-26 13:40:12 +0700

    Update sha.py

--- sha.py
+++ sha.py
@@ -4,22 +4,30 @@ import hashlib
 def sha():
     input_text = st.text_area("Masukan text")
     choice = st.selectbox("SHA :", ["SHA256","SHA384","SHA224","SHA512","SHA1"])
-    if st.button("Hash"):
-        if len(input_text) == 0:
-            st.error("Masukan Text!")
-        else:
-            if choice == "SHA256":
-                result = hashlib.sha256(input_text.encode())
-                st.info(result.hexdigest())
-            if choice == "SHA384":
-                result = hashlib.sha384(input_text.encode())
-                st.info(result.hexdigest()) 
-            if choice == "SHA224":
-                result = hashlib.sha224(input_text.encode())
-                st.info(result.hexdigest())
-            if choice == "SHA512":
-                result = hashlib.sha512(input_text.encode())
-                st.info(result.hexdigest())
-            if choice == "SHA1":
-                result = hashlib.sha1(input_text.encode())
-                st.info(result.hexdigest())
+    col1,col2 = st.columns([14,1])
+    with col2:
+        if st.button("Hash"):
+            if len(input_text) == 0:
+                with col1:
+                    st.error("Masukan Text!")
+            else:
+                if choice == "SHA256":
+                    result = hashlib.sha256(input_text.encode())
+                    with col1:
+                        st.info(result.hexdigest())
+                if choice == "SHA384":
+                    result = hashlib.sha384(input_text.encode())
+                    with col1:
+                            st.info(result.hexdigest()) 
+                if choice == "SHA224":
+                    result = hashlib.sha224(input_text.encode())
+                    with col1:
+                        st.info(result.hexdigest())
+                if choice == "SHA512":
+                    result = hashlib.sha512(input_text.encode())
+                    with col1:  
+                        st.info(result.hexdigest())
+                if choice == "SHA1":
+                    result = hashlib.sha1(input_text.encode())
+                    with col1:
+                        st.info(result.hexdigest())
