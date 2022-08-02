4332c547a475122343ae8ecb598a30623f487f86
windayanis <109908587+windayanis@users.noreply.github.com>
2022-08-02 19:54:19 +0700

    Update base_64.py

--- base_64.py
+++ base_64.py
@@ -2,13 +2,13 @@ import streamlit as st
 import base64
 
 def base_64():
-    input1 = st.text_area("Masukan string")
+    input1 = st.text_area("Masukkan PlainText")
     col1,col2,col3 = st.columns([10,1,1])
     with col2:
         if st.button("Encode"):
             if len(input1) == 0:
                 with col1:
-                    st.error("Masukan Text!")
+                    st.error("Masukkan Text!")
             else:
                 hasil = input1.encode("ascii")
                 base64_bytes = base64.b64encode(hasil)
@@ -21,13 +21,34 @@ def base_64():
             #a = input1 + '=' * (4 - len(input1) % 4)
             if len(input1) % 4 != 0:
                 with col1:
-                    st.error("Masukan yang benar!")
+                    st.error("Masukkan yang benar!")
             elif len(input1) == 0:
                 with col1:
-                    st.error("Masukan Text!")
+                    st.error("Masukkan Text!")
             else:
                 hasil = input1.encode("ascii")
                 sample_string_bytes = base64.b64decode(hasil)
                 sample_string = sample_string_bytes.decode("ascii")
                 with col1:
                     st.info(sample_string)
+
+    with st.container():
+        st.text("")
+        st.header("Apa itu Base64?")
+        st.write("""
+                Base64 merupakan salah satu algoritma untuk encoding dan decoding suatu data ke dalam format ASCII, 
+                yang didasarkan pada bilangan dasar 64 atau bisa dikatakan sebagai
+                salah satu metode yang digunakan untuk
+                melakukan encoding (penyandian) terhadap data
+                binary. 
+                
+                Karakter yang dihasilkan pada
+                transformasi Base64 ini terdiri dari A….Z, a….z
+                dan 0….9, serta ditambah dengan dua karakter
+                terakhir yang bersimbol yaitu + dan / serta satu
+                buah karakter sama dengan (=) yang digunakan
+                untuk penyesuaian dan menggenapkan data
+                binary atau istilahnya disebut sebagai pengisi pad.
+                Karakter simbol yang akan dihasilkan akan
+                tergantung dari proses algoritma yang berjalan.              
+                """)
