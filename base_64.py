a57007cba00f08d947e442f7554022a7e4605491
windayanis <109908587+windayanis@users.noreply.github.com>
2022-08-02 20:29:56 +0700

    Update base_64.py

--- base_64.py
+++ base_64.py
@@ -17,20 +17,20 @@ def base_64():
                 with col1:
                     st.info(base64_string)
     with col3:
-        if st.button("Decode"):
-            #a = input1 + '=' * (4 - len(input1) % 4)
-            if len(input1) % 4 != 0:
-                with col1:
-                    st.error("Masukkan yang benar!")
-            elif len(input1) == 0:
-                with col1:
-                    st.error("Masukkan Text!")
-            else:
-                hasil = input1.encode("ascii")
-                sample_string_bytes = base64.b64decode(hasil)
-                sample_string = sample_string_bytes.decode("ascii")
-                with col1:
-                    st.info(sample_string)
+        try:
+            if st.button("Decode"):
+                if len(input1) == 0:
+                    with col1:
+                        st.error("Masukkan Text!")
+                else:
+                    hasil = input1.encode("ascii")
+                    sample_string_bytes = base64.b64decode(hasil)
+                    sample_string = sample_string_bytes.decode("ascii")
+                    with col1:
+                        st.info(sample_string)
+        except:
+            with col1:
+                st.error("Kesalahan Input")
 
     with st.container():
         st.text("")
